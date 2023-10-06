ae514ee7144c940bc5f2dc3a98f8c37f21fd65ad
Simon Castillo <simocastles@gmail.com>
2023-10-06 10:52:46 -0300

    dy98q7d

--- js/featuresTab.js
+++ js/featuresTab.js
@@ -52,15 +52,15 @@ function createTab(imgSrc, title, content) {
 function updateTab(tabId) {
    let tab;
    if (tabId == 1) tab = createTab(
-      "https://raw.githubusercontent.com/devsimocastles/bookmark-landing-page/a98cc62b95d6ac4f6490e4cb0642a27a730a398c/images/illustration-features-tab-1.svg",
+      "../images/illustration-features-tab-1.png",
       "Bookmark in one click",
       "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites");
    if (tabId== 2) tab = createTab(
-      "https://raw.githubusercontent.com/devsimocastles/bookmark-landing-page/a98cc62b95d6ac4f6490e4cb0642a27a730a398c/images/illustration-features-tab-2.svg",
+      "../images/illustration-features-tab-2.png",
       "Intelligent search", 
       "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.");
    if (tabId== 3) tab = createTab(
-      "https://github.com/devsimocastles/bookmark-landing-page/blob/main/images/illustration-features-tab-3.svg",
+      "../images/illustration-features-tab-3.png",
       "Share your bookmarks",
       "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button."
    ) 
